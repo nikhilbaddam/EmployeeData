0a3d1ad14c2f47e981dd84d67ab72fe412948f93
nikhil <91113985+nikhilbaddam@users.noreply.github.com>
2024-11-12 00:48:43 +0530

    Update App.jsx
    changes for employee list

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import {Route,Routes} from 'react-router-dom'
 import Register from './components/Register/Register'
 import Login from './components/Login'
 import Navbar from './components/Navbar'
-import EmployeeList from './pages/employeeList'
+import EmployeeList from './pages/EmployeeList'
 import Home from './pages/Home'
 const App = () => {
   return (
